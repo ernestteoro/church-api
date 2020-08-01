077b7a0983a51376b3230ea28097ba653d4d39e4
ernestteoro <47726097+ernestteoro@users.noreply.github.com>
2020-08-01 02:48:37 +0000

    Update church-server.js

--- church-server.js
+++ church-server.js
@@ -1,9 +1,9 @@
 const http  = require('http');
 const app = require('./church-app');
 
-const port  = process.env.port || 8180;
+const port  = process.env.PORT || 8180;
 const server  = http.createServer(app);
 
 server.listen(port,()=>{
-    console.log(' The app is running; make request for data')
+    //console.log(' The app is running; make request for data')
 })
